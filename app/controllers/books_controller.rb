501edfbb91062e591cf7185da0ee66a186dd7eb8
Pooja Bhore <psbhore@mitaoe.ac.in>
2021-09-13 12:43:28 +0600

    Update books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,45 +1,42 @@
 class BooksController < ApplicationController
  
- def index
+  def index
     @current_user = User.find(session[:user_id])    
-    @books = Book.all
-    
-    
- end   
+    @books = Book.all    
+  end   
 
   
  
- def show   
+  def show   
     @current_user = User.find(session[:user_id]) 
     @book = Book.find(params[:id])  
-    @issued_books = IssuedBook.find_by(book_id: @book.id)
-  
- end   
+    @issued_books = IssuedBook.find_by(book_id: @book.id)  
+  end   
   
    
- def new   
-   @book = Book.new 
- end   
+  def new   
+    @book = Book.new 
+  end   
   
   
- def create   
-   @book = Book.new(book_params)   
-   if @book.save   
-     flash[:notice] = 'Book added!'   
-     redirect_to books_path   
-   else   
-     flash[:error] = 'Failed to edit book!'   
-     render :new   
-   end   
- end 
+  def create   
+    @book = Book.new(book_params)   
+    if @book.save   
+      flash[:notice] = 'Book added!'   
+      redirect_to books_path   
+    else   
+      flash[:error] = 'Failed to edit book!'   
+      render :new   
+    end   
+  end 
 
- def borrow
-  @book = Book.find(params[:id])
-  @book.is_borrowed = true
+  def borrow
+    @book = Book.find(params[:id])
+    @book.is_borrowed = true
     @book.user_id = session[:user_id]
     @issued_book = IssuedBook.find_by(book_id: @book.id)
 
-    if @book.save!
+    if @book.save! 
       @issued_book.update_columns(issue_date: DateTime.now)
       redirect_to @book, notice: 'Book is successfully borrowed.'
     else 
@@ -48,6 +45,7 @@ class BooksController < ApplicationController
     end
   end
 
+  
   def return
     @book=Book.find(params[:id])  
     @book.return = true
@@ -72,38 +70,38 @@ class BooksController < ApplicationController
   
   
     
- def edit   
-   @book = Book.find(params[:id])   
+  def edit   
+    @book = Book.find(params[:id])   
+  end   
+  
+   
+  def update   
+    @book = Book.find(params[:id])  
+    if @book.update_attributes(book_params)   
+      flash[:notice] = 'Book updated!'   
+      redirect_to books_path   
+    else   
+      flash[:error] = 'Failed to edit book!'   
+      render :edit   
+    end   
  end   
   
    
- def update   
-   @book = Book.find(params[:id])  
-   if @book.update_attributes(book_params)   
-     flash[:notice] = 'Book updated!'   
-     redirect_to books_path   
-   else   
-     flash[:error] = 'Failed to edit book!'   
-     render :edit   
-   end   
- end   
-  
-   
- def destroy   
-   @book = Book.find(params[:id])   
-   if @book.delete   
-     flash[:notice] = 'Book deleted!'   
-     redirect_to books_path   
-   else   
-     flash[:error] = 'Failed to delete this book!'   
-     render :destroy   
-   end   
+  def destroy   
+    @book = Book.find(params[:id])   
+    if @book.delete   
+      flash[:notice] = 'Book deleted!'   
+      redirect_to books_path   
+    else   
+      flash[:error] = 'Failed to delete this book!'   
+      render :destroy   
+    end   
  end 
  
  
   
- def book_params   
-   params.require(:book).permit(:book_name, :desc, :is_borrowed, :issue_date, :return_date)    
- end  
+  def book_params   
+    params.require(:book).permit(:book_name, :desc, :is_borrowed, :issue_date, :return_date)    
+  end  
 
 end
